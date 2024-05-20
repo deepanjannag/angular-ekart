a255b645da212d5bb0a6f091eee7c7bc72f371ac
deepanjan.nag <deepanjan.nag@gmail.com>
2024-05-20 21:39:59 +0000

    add and delete files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { SearchComponent } from './product-list/search/search.component';
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TopHeaderComponent, TopMenuComponent, MainMenuComponent, ProductListComponent, SearchComponent],
+  declarations: [AppComponent, HeaderComponent, TopHeaderComponent, TopMenuComponent, MainMenuComponent, ContainerComponent, SearchComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
